fe9510d5dff4b99c3cc94b05c86c01812d1c4eec
Julia <ruzhytskya1997@gmail.com>
2022-02-08 13:29:00 +0200

    WA-1 Create project & connect with GitHub

---
+++ lib/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(backgroundColor: Colors.cyan, title: Text(widget.title)),
+        body: Container());
+  }
+}
